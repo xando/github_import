b9b122e0f30ebac778207b20bf5b078459a63352
xando <sebastian.pawlus@gmail.com>
2012-07-21 13:21:52 +0100

    classifiers fixed

--- setup.py
+++ setup.py
@@ -18,11 +18,9 @@ setup(name='github_import',
       platforms=['any'],
       classifiers=[
         'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Framework :: Django',
     ],
 )
